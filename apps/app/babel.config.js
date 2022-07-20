8b54bb75abbfcd88b615b51b6da0c0d5b944cbe2
Amos <amosbastian@gmail.com>
2022-07-21 00:57:16 +0200

    feat(app): set up tamagui

--- apps/app/babel.config.js
+++ apps/app/babel.config.js
@@ -2,6 +2,21 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+        },
+      ],
+      [
+        'transform-inline-environment-variables',
+        {
+          include: 'TAMAGUI_TARGET',
+        },
+      ],
+    ],
   };
 };
--- apps/app/src/app/App.tsx
+++ apps/app/src/app/App.tsx
@@ -1,8 +1,17 @@
 import { NativeNavigation } from '@nx-expo-nextjs/navigation/native';
 import { Provider } from '@nx-expo-nextjs/shared/provider';
 import React from 'react';
+import { useFonts } from 'expo-font';
 
 const App = () => {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/ttf/Inter.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Provider>
       <NativeNavigation />
